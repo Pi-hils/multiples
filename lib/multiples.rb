22ab6faf563fd085d9d73c2f936cab0640d91b66
Hilda Amponsah <hilda.amp@hotmail.com>
2021-02-19 16:02:34 +0000

    outputs mmultiple of 8 to up 200

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,6 +1,26 @@
 class Multiples
-  def multiple_of_7(arr, num)
-    7
+
+  def multiple_of_7(num)
+
+    7.step(100,7) do |num|
+      puts "number is #{num}"
+    end
+    
+  end
+
+  def multiple_of_8(num)
+
+    8.step(200,8) do |num|
+      puts "number is #{num}"
+    end
+  end
+
+
+  def multiple_of_9(num)
+
+    9.step(300,9) do |num|
+      puts "number is #{num}"
+    end
   end
 
 end
--- spec/multiples_spec.rb
+++ spec/multiples_spec.rb
@@ -2,8 +2,16 @@ require 'multiples'
 
 describe Multiples do
   #when multiplied by 7, ouputs nnumber
-  it 'if divides  perfectly into 7 it returns 7' do
-    expect(subject.multiple_of_7(7,7)).to eq(7)
+  it 'if divided perfectly into 7 it returns 7' do
+    expect(subject.multiple_of_7(7)).to eq(7)
+  end
+
+  it"8 divides perfectly into the multiple will return the multiple" do
+    expect(subject.multiple_of_8(8)).to eq(8)
+  end
+  
+  it "9 divides perfectly into 7 so will return 14" do
+    expect(subject.multiple_of_9(9)).to eq(9)
   end
 
 end
