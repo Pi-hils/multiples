29e7db7207181fae02cbcbc4daa90332fd39288a
Hilda Amponsah <hilda.amp@hotmail.com>
2021-03-01 14:58:36 +0000

    revisiting, new approach and refactoring multiples

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,26 +1,26 @@
 class Multiples
 
-  def multiple_of_7(num)
+  def multiple_of_7(num,limit)
 
-    7.step(100,7) do |num|
-      puts "#{num}"
-    end
-    
-  end
-
-  def multiple_of_8(num)
-
-    8.step(200,8) do |num|
-      puts "#{num}"
+    for i in (7..100).step(7) do
+       i
     end
   end
 
 
-  def multiple_of_9(num)
+  # def multiple_of_8(num)
 
-    9.step(300,9) do |num|
-      puts "#{num}"
-    end
-  end
+  #   8.step(200,8) do |num|
+  #     puts "#{num}"
+  #   end
+  # end
+
+
+  # def multiple_of_9(num)
+
+  #   9.step(300,9) do |num|
+  #     puts "#{num}"
+  #   end
+  # end
 
 end
--- spec/multiples_spec.rb
+++ spec/multiples_spec.rb
@@ -2,16 +2,20 @@ require 'multiples'
 
 describe Multiples do
   #when multiplied by 7, ouputs nnumber
-  it 'if divided perfectly into 7 it returns 7' do
-    expect(subject.multiple_of_7(7)).to eq(7)
+  it 'if divided perfectly into 7 it returns multiples of 7' do
+    expect(subject.multiple_of_7(7,100)).to include(7)
   end
 
-  it"8 divides perfectly into the multiple will return the multiple" do
-    expect(subject.multiple_of_8(8)).to eq(8)
+  it 'if divided perfectly into 7 it returns multiples of 7' do
+    expect(subject.multiple_of_7(7,100)).to include(14)
   end
+
+  # it"8 divides perfectly into the multiple will return the multiple" do
+  #   expect(subject.multiple_of_8(8)).to eq(8)
+  # end
   
-  it "9 divides perfectly into 7 so will return 14" do
-    expect(subject.multiple_of_9(9)).to eq(9)
-  end
+  # it "9 divides perfectly into 9 so will return th multiple" do
+  #   expect(subject.multiple_of_9(9)).to eq(9)
+  # end
 
 end
